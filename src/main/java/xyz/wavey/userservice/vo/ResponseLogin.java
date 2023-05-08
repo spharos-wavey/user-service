649987f62463bbf1dfaf11996c5d3e96b890c782
minji <kmj99a@gmail.com>
2023-05-08 11:17:27 +0900

    [Create] ResponseLogin vo 생성

---
+++ src/main/java/xyz/wavey/userservice/vo/ResponseLogin.java
@@ -0,0 +1,14 @@
+package xyz.wavey.userservice.vo;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class ResponseLogin {
+    private String email;
+    private String name;
+    private String ageRange;
+    private String phoneNumber;
+    private String nickName;
+}
