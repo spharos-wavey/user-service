de3c8629e7e1a437f3bc61424d1625323d9af9c8
minji <kmj99a@gmail.com>
2023-05-08 16:03:05 +0900

    [Delete] 회원가입 선택 동의 항목

--- src/main/java/xyz/wavey/userservice/vo/ResponseLogin.java
+++ src/main/java/xyz/wavey/userservice/vo/ResponseLogin.java
@@ -7,9 +7,6 @@ import lombok.Data;
 @Builder
 public class ResponseLogin {
     private String email;
-    private String name;
-    private String ageRange;
-    private String phoneNumber;
     private String nickName;
     private String profileImageUrl;
 }
