785f25e5892eeb4fb656fb61367562f40eb63922
minji <kmj99a@gmail.com>
2023-05-08 11:25:14 +0900

    [Update] User 컬럼 수정

--- src/main/java/xyz/wavey/userservice/model/User.java
+++ src/main/java/xyz/wavey/userservice/model/User.java
@@ -18,7 +18,7 @@ public class User {
     private Long id;
     private String name;
     private String email;
-    private String pwd;
+    private String profileImage;
     private String phoneNum;
     private Boolean blockList;
     private String UUID;
