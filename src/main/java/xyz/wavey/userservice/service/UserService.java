60ba13273c49440891e724082d2dbd8ba33bd13d
minji <kmj99a@gmail.com>
2023-05-09 12:22:38 +0900

    [Create] Login 로직

--- src/main/java/xyz/wavey/userservice/service/UserService.java
+++ src/main/java/xyz/wavey/userservice/service/UserService.java
@@ -1,9 +1,11 @@
 package xyz.wavey.userservice.service;
 
+import org.springframework.http.ResponseEntity;
+import xyz.wavey.userservice.vo.RequestLogin;
 import xyz.wavey.userservice.vo.ResponseLogin;
 
 public interface UserService {
 
     void userValid(ResponseLogin responseLogin, String userId);
-
+    ResponseEntity<Object> login(RequestLogin requestLogin);
 }
