758281e2ac9762901d3c851f74f00f91a5eb591b
minji <kmj99a@gmail.com>
2023-05-09 16:40:44 +0900

    [Create] 토큰 관련 에러 코드 생성

--- src/main/java/xyz/wavey/userservice/base/exception/ErrorCode.java
+++ src/main/java/xyz/wavey/userservice/base/exception/ErrorCode.java
@@ -4,12 +4,16 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import org.springframework.http.HttpStatus;
 
-import static org.springframework.http.HttpStatus.NOT_FOUND;
+import static org.springframework.http.HttpStatus.*;
+import static org.springframework.http.HttpStatus.BAD_REQUEST;
 
 @Getter
 @AllArgsConstructor
 public enum ErrorCode {
     NOT_FOUND_USER("회원 정보가 존재하지 않습니다.", NOT_FOUND, 404),
+    INVALID_AUTH_TOKEN("권한 정보가 없는 토큰입니다", UNAUTHORIZED, 401),
+    INVALID_TOKEN("토큰이 유효하지 않습니다", BAD_REQUEST, 400),
+    INVALID_ACCESS("유효하지 않은 접근입니다.", NOT_FOUND, 404),
     ;
     private final String message;
     private final HttpStatus httpStatus;
