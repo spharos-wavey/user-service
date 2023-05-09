a3b781654e63db3d2b28f5560ebebdff7cb1dfba
minji <kmj99a@gmail.com>
2023-05-09 17:50:11 +0900

    [Delete] UserRepo 메소드 삭제

--- src/main/java/xyz/wavey/userservice/repository/UserRepo.java
+++ src/main/java/xyz/wavey/userservice/repository/UserRepo.java
@@ -7,6 +7,5 @@ import java.util.Optional;
 
 public interface UserRepo extends JpaRepository<User,Long> {
     Boolean existsByEmail(String email);
-    Optional<User> findByUserId(String userId);
 
 }
