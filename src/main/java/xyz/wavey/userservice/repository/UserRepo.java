8aaece3dbb3f18ffd1d87bd57cd5aaad8fbb1cb4
minji <kmj99a@gmail.com>
2023-05-09 16:36:29 +0900

    [Create] findByUserId 메소드

--- src/main/java/xyz/wavey/userservice/repository/UserRepo.java
+++ src/main/java/xyz/wavey/userservice/repository/UserRepo.java
@@ -3,6 +3,10 @@ package xyz.wavey.userservice.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import xyz.wavey.userservice.model.User;
 
+import java.util.Optional;
+
 public interface UserRepo extends JpaRepository<User,Long> {
     Boolean existsByEmail(String email);
+    Optional<User> findByUserId(String userId);
+
 }
