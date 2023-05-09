147a54e8a1b24eff1099de35713094cd7046aae5
minji <kmj99a@gmail.com>
2023-05-09 12:22:59 +0900

    [Create] Login 서비스

--- src/main/java/xyz/wavey/userservice/service/UserServiceImpl.java
+++ src/main/java/xyz/wavey/userservice/service/UserServiceImpl.java
@@ -2,10 +2,15 @@ package xyz.wavey.userservice.service;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import xyz.wavey.userservice.model.User;
 import xyz.wavey.userservice.repository.UserRepo;
+import xyz.wavey.userservice.vo.RequestLogin;
 import xyz.wavey.userservice.vo.ResponseLogin;
+
+import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
@@ -22,5 +27,20 @@ public class UserServiceImpl implements UserService{
                     .nickName(responseLogin.getNickName())
                     .build());
         }
+    }
+
+    @Override
+    public ResponseEntity<Object> login(RequestLogin requestLogin) {
+        if (Boolean.FALSE.equals(userRepo.existsByEmail(requestLogin.getEmail()))) { //디비에 존재 안하면
+            UUID uuid = UUID.randomUUID();
+            userRepo.save(User.builder()
+                    .email(requestLogin.getEmail())
+                    .profileImageUrl(requestLogin.getProfileImageUrl())
+                    .UUID(uuid.toString())
+                    .nickName(requestLogin.getNickName())
+                    .build());
+            return ResponseEntity.status(HttpStatus.OK).build();
+        }
+        return null;
     }
 }
