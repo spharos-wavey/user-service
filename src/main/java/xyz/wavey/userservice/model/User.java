0d9c91b150f13754139c0cafa41f3042ad6740ff
minji <kmj99a@gmail.com>
2023-05-09 14:52:30 +0900

    [Create] User -> BaseTimeEntity 적용

--- src/main/java/xyz/wavey/userservice/model/User.java
+++ src/main/java/xyz/wavey/userservice/model/User.java
@@ -5,6 +5,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.*;
+import xyz.wavey.userservice.base.BaseTimeEntity;
 
 @Entity
 @AllArgsConstructor
@@ -12,7 +13,7 @@ import lombok.*;
 @Getter
 @Setter
 @Builder
-public class User {
+public class User extends BaseTimeEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
