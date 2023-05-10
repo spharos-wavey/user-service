e4a7fda5fc3af2d7bec3ef08ceb86c7ed587e15d
Ji-Wook, Kim <20korea01@gmail.com>
2023-05-10 10:44:11 +0900

    [Delete] 로그인 방식 변경으로 인해 기존 OAuth와 관련된 코드들 삭제

--- src/main/java/xyz/wavey/userservice/service/OAuthService.java
+++ src/main/java/xyz/wavey/userservice/service/OAuthService.java
@@ -2,14 +2,9 @@ package xyz.wavey.userservice.service;
 
 import org.springframework.http.ResponseEntity;
 import xyz.wavey.userservice.vo.RequestLogin;
-import xyz.wavey.userservice.vo.ResponseLogin;
-import xyz.wavey.userservice.vo.ResponseGetToken;
 
 public interface OAuthService {
-    ResponseGetToken getAccessToken(String code);
-    ResponseLogin getUserInfo(String accessToken);
-    String decodeToken(String jwt);
 
-    void userValid(ResponseLogin responseLogin, String userId);
     ResponseEntity<Object> login(RequestLogin requestLogin);
+
 }
