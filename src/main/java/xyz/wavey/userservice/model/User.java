8bef3fe2f2d7af809c8b70be6f2bcc2824780364
minji <kmj99a@gmail.com>
2023-05-10 12:12:35 +0900

    [Create] UserDetail imple

--- src/main/java/xyz/wavey/userservice/model/User.java
+++ src/main/java/xyz/wavey/userservice/model/User.java
@@ -2,7 +2,11 @@ package xyz.wavey.userservice.model;
 
 import jakarta.persistence.*;
 import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 import xyz.wavey.userservice.base.BaseTimeEntity;
+
+import java.util.Collection;
 
 @Entity
 @AllArgsConstructor
@@ -10,7 +14,7 @@ import xyz.wavey.userservice.base.BaseTimeEntity;
 @Getter
 @Setter
 @Builder
-public class User extends BaseTimeEntity {
+public class User extends BaseTimeEntity implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -26,4 +30,39 @@ public class User extends BaseTimeEntity {
     private String nickName;
     private String secession;
     private Integer reward;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getPassword() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return uuid;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
+    }
 }
